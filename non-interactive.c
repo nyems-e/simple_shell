6c15848ed9d315205a3a9b7de881e42dbe5dbe6f
Jesse <ebennyems@gmail.com>
2023-11-26 12:13:22 -0800

    1

--- non-interactive.c
+++ non-interactive.c
@@ -32,7 +32,6 @@ int nonInter_run(char **tokArray, size_t tokCount, char *argv,
 {
 	size_t count = check_Array(tokArray);
 	size_t i = 0;
-	char *custom_arr[2];
 
 	while (i < count)
 	{
@@ -49,9 +48,7 @@ int nonInter_run(char **tokArray, size_t tokCount, char *argv,
 		}
 		if (main_helper(tokArray, tokCount, line_num, argv, status) == 1)
 			continue;
-		custom_arr[0] = tokArray[0];
-		custom_arr[1] = NULL;
-		*status = runCommand(tokArray[i - 1], custom_arr, tokCount);
+		*status = runCommand(tokArray[i - 1], tokArray, tokCount);
 		if (*status != 0)
 		{
 			func_free(tokArray, tokCount);
